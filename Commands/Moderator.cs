a1b1e165e4a0193ad043943b992ab5984eb3924e
trentb10 <trentbmin@gmail.com>
2023-10-29 00:07:03 -0400

    Added ability to sent a message into another channel using d-say, needs work still

--- Commands/Moderator.cs
+++ Commands/Moderator.cs
@@ -1,4 +1,5 @@
 using System.Text;
+using System.Collections.Generic;
 using System.Web;
 using DSharpPlus;
 using DSharpPlus.CommandsNext;
@@ -11,11 +12,27 @@ using Newtonsoft.Json;
 public class Moderator : BaseCommandModule
 {
   [Command("say"), RequireUserPermissions(DSharpPlus.Permissions.ModerateMembers)]
-  public async Task DSay(CommandContext ctx, [RemainingText] string message = "")
+  public async Task DSay(CommandContext ctx, string channel, [RemainingText] string message = "")
   {
-    if (message != "")
+    if (channel != "")
     {
-      await ctx.Channel.SendMessageAsync(message);
+      try
+      {
+        // First detect if the channel is actually a channel
+        string[] filter = new string[] { "<", ">", "#" };
+        foreach (string c in filter)
+        {
+          channel = channel.Replace(c, string.Empty);
+        }
+        DiscordChannel chan = await ctx.Client.GetChannelAsync(Convert.ToUInt64(channel));
+        
+        // Valid channel, post message in designated channel
+        await chan.SendMessageAsync(message);
+      }
+      catch
+      {
+        await ctx.Channel.SendMessageAsync($"{channel} {message}");
+      }
     } 
     else
     {
